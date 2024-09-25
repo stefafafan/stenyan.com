81a1766db59768c4999626a6e7d381188c79a6b0
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-09-25 22:39:51 +0900

    quotations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import devServer from '@hono/vite-dev-server';
 import adapter from '@hono/vite-dev-server/cloudflare';
-import ssg from "@hono/vite-ssg";
+import ssg from '@hono/vite-ssg';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
