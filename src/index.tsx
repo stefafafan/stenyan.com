a5cf7dea937c7e3d1b5500494c51d676ccc3012e
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-09-29 15:42:11 +0900

    serve static images

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 import { TopPage } from "./pages/TopPage";
 import { renderer } from "./renderer";
@@ -5,6 +6,8 @@ import { renderer } from "./renderer";
 const app = new Hono();
 
 app.use(renderer);
+
+app.use("*", serveStatic({ root: "./static" }));
 
 app.get("/", (c) => {
 	return c.render(TopPage());
