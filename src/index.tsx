fb6862e42633b3625361749eca48ea2b24a3dd2a
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-09-29 15:30:30 +0900

    refactor: move toppage into src/pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { css } from "hono/css";
+import { Index } from "./pages";
 import { renderer } from "./renderer";
 
 const app = new Hono();
@@ -7,71 +7,7 @@ const app = new Hono();
 app.use(renderer);
 
 app.get("/", (c) => {
-	const headerStyle = css`
-		display: flex;
-		align-items: center;
-	`;
-	const h1Style = css`
-		margin-left: 10px;
-	`;
-	return c.render(
-		<>
-			<header>
-				<div class={headerStyle}>
-					<img
-						src="static/images/stefafafan.png"
-						alt="stefafafan"
-						style={{ height: "3em", verticalAlign: "middle" }}
-					/>
-					<h1 class={h1Style}>stefafafan a.k.a. すてにゃん</h1>
-				</div>
-				<p>すてにゃん (stefafafan) の個人サイトです。</p>
-			</header>
-			<section>
-				<h2>各種SNS</h2>
-				<ul>
-					<li>
-						<a href="https://x.com/stefafafan" target="_blank" rel="noreferrer">
-							𝕏 (Formerly Twitter)
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://github.com/stefafafan"
-							target="_blank"
-							rel="noreferrer"
-						>
-							GitHub
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://speakerdeck.com/stefafafan"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Speaker Deck
-						</a>
-					</li>
-					<li>
-						<a href="https://blog.stenyan.jp/" target="_blank" rel="noreferrer">
-							Hatena Blog
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://b.hatena.ne.jp/stefafafan/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Hatena Bookmark
-						</a>
-					</li>
-				</ul>
-			</section>
-			<footer>&copy; 2024 すてにゃん</footer>
-		</>,
-	);
+	return c.render(Index());
 });
 
 export default app;
---
+++ src/pages/Index.tsx
@@ -0,0 +1,69 @@
+import { css } from "hono/css";
+
+export const Index = () => {
+	const headerStyle = css`
+		display: flex;
+		align-items: center;
+	`;
+	const h1Style = css`
+		margin-left: 10px;
+	`;
+	return (
+		<>
+			<header>
+				<div class={headerStyle}>
+					<img
+						src="static/images/stefafafan.png"
+						alt="stefafafan"
+						style={{ height: "3em", verticalAlign: "middle" }}
+					/>
+					<h1 class={h1Style}>stefafafan a.k.a. すてにゃん</h1>
+				</div>
+				<p>すてにゃん (stefafafan) の個人サイトです。</p>
+			</header>
+			<section>
+				<h2>各種SNS</h2>
+				<ul>
+					<li>
+						<a href="https://x.com/stefafafan" target="_blank" rel="noreferrer">
+							𝕏 (Formerly Twitter)
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://github.com/stefafafan"
+							target="_blank"
+							rel="noreferrer"
+						>
+							GitHub
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://speakerdeck.com/stefafafan"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Speaker Deck
+						</a>
+					</li>
+					<li>
+						<a href="https://blog.stenyan.jp/" target="_blank" rel="noreferrer">
+							Hatena Blog
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://b.hatena.ne.jp/stefafafan/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Hatena Bookmark
+						</a>
+					</li>
+				</ul>
+			</section>
+			<footer>&copy; 2024 すてにゃん</footer>
+		</>
+	);
+};
