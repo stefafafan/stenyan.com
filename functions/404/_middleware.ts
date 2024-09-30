6ff9d90cb06b61b878f9d99a2f3e9f5ef94efb6f
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-09-30 23:25:07 +0900

    GETとHEADのみ404を返すように

--- functions/404/_middleware.ts
+++ functions/404/_middleware.ts
@@ -1,6 +1,11 @@
 // /404 にきたリクエストのステータスコードを 404 で上書きする
 // ref. https://zenn.dev/mogamin/articles/override-status-cloudflare-pages-functions
-export const onRequest: PagesFunction = async (context) => {
+export const onRequestGet: PagesFunction = async (context) => {
+	const original = await context.env.ASSETS.fetch(context.request);
+	return new Response(original.body, { status: 404 });
+};
+
+export const onRequestHead: PagesFunction = async (context) => {
 	const original = await context.env.ASSETS.fetch(context.request);
 	return new Response(original.body, { status: 404 });
 };
